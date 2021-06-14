6cd605fd82c20baa6d25c3dfe1fd537efb34f591
danilo-mart <daniloms@msn.com>
2021-06-13 23:54:41 -0300

    api/login

--- controllers/LoginController.py
+++ controllers/LoginController.py
@@ -1,15 +1,11 @@
 from flask import Blueprint, request, Response
 from flask_restx import Namespace, Resource, fields
-
-
-import config
 from dtos.ErroDTO import ErroDTO
-
-import json
-
 from dtos.UsuarioDTO import UsuarioLoginDTO
 from services import JWTService
 from services.UsuarioService import UsuarioService
+
+import json
 
 login_controller = Blueprint('login_controller', __name__)
 
@@ -20,20 +16,19 @@ login_fields = api.model('LoginDTO', {
     'senha': fields.String
 })
 
-user_fields = api.model('UsuarioDTO',{
+user_fields = api.model('UsuarioDTO', {
     'name': fields.String,
     'email': fields.String,
-    'token': fields.String
+    'token': fields.String,
 })
 
 @api.route('/login', methods=['POST'])
 class Login(Resource):
-    @api.doc(responses={200:'Login realizado com sucesso.'})
+    @api.doc(responses={200: 'Login realizado com sucesso.'})
     @api.doc(responses={400: "Parâmetros de entrada inválidos."})
-    @api.doc(responses={500: 'Não foi possível efetuar o login, tente novamente.'})
+    @api.doc(responses={500: "Não foi possível efetuar o login, tente novamente"})
     @api.response(200, 'Success', user_fields)
     @api.expect(login_fields)
-
     def post(self):
         try:
             body = request.get_json()
@@ -42,27 +37,29 @@ class Login(Resource):
 
             if not body:
                 return Response(
-                    json.dumps(ErroDTO(400, "Body da requisição está vazio").__dict__),
+                    json.dumps(ErroDTO(400, "Body da requisição está vazio.").__dict__),
                     status=400,
                     mimetype='application/json'
                 )
 
             if not "login" in body:
-                erros.append("Campo 'login' é obrigatorio.")
+                erros.append("Campo 'login' é obrigatório.")
 
             if not "senha" in body:
-                erros.append(("Campo 'senha' é obrigatorio."))
+                erros.append("Campo 'senha' é obrigatório.")
 
             if erros:
                 return Response(
-                                json.dumps(ErroDTO(400, erros).__dict__),
-                                status=400,
-                                mimetype='application/json'
-                                )
-            usuario_encontrado = UsuarioService().login(body['login'], body['senha'])
+                    json.dumps(ErroDTO(400, erros).__dict__),
+                    status=400,
+                    mimetype='application/json'
+                )
+
+            usuario_encontrado = UsuarioService().login(body["login"], body["senha"])
 
             if usuario_encontrado:
                 token = JWTService.gerar_token(usuario_encontrado.id)
+
                 return Response(
                     json.dumps(UsuarioLoginDTO(usuario_encontrado.nome, usuario_encontrado.email, token).__dict__),
                     status=200,
@@ -70,14 +67,12 @@ class Login(Resource):
                 )
 
             return Response(
-                json.dumps(ErroDTO(401, 'Usuário ou senha incorretos, favor tentar novamente').__dict__),
-                status=200,
-                mimetype='application/json'
-             )
-
+                json.dumps(ErroDTO(401, 'Usuário ou Senha incorretos, favor tentar novamente.').__dict__),
+                status=401,
+                mimetype='application/json')
         except Exception:
             return Response(
-                            json.dumps(ErroDTO(500, 'Não foi possível efetuar o login, tente novamente').__dict__),
-                            status=500,
-                            mimetype='application/jason'
-                            )
+                json.dumps(ErroDTO(500, "Não foi possível efetuar o login, tente novamente.").__dict__),
+                status=500,
+                mimetype='application/json'
+            )
